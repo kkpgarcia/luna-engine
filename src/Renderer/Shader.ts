1f8a321d096df91ed7e223f31eff8e3a0b4b78c2
Kyle Garcia <kkpgarcia@gmail.com>
2021-07-02 22:36:08 +0800

    Added texture in game engine

--- src/Renderer/Shader.ts
+++ src/Renderer/Shader.ts
@@ -76,6 +76,12 @@ export default class Shader
         gl.useProgram(null);
     }
 
+    public SetUnitform1i(name: string, data: number): void
+    {
+        const gl = RenderingContext.instance.gl;
+        gl.uniform1i(this.GetUniformLocation(name), data);
+    }
+
     public SetUniform4f(name: string, data: number[]): void
     {
         const gl = RenderingContext.instance.gl;
